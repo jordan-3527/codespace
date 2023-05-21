9b1cdf555a7cefdc54257ba90d52a6d82f65100b
jordan-3527 <126618762+jordan-3527@users.noreply.github.com>
2023-05-21 01:35:05 +0000

    Sun, May 21, 2023, 1:35 AM +00:00

--- population/population.c
+++ population/population.c
@@ -5,9 +5,33 @@ int main(void)
 {
     // TODO: Prompt for start size
 
+    int start_size;
+    do
+    {
+        start_size = get_int("Starting Population Size: ")
+    }
+    while(start_size < 9);
+
     // TODO: Prompt for end size
+
+    int end_size;
+    do
+    {
+        end_size = get_int("Ending Population Size: ")
+    }
+    while(start size > end_size);
 
     // TODO: Calculate number of years until we reach threshold
 
+    int years = 0
+    do
+    {
+        start_size = start_size + (start_size/3) - (start_size/4);
+        years++
+    }
+    while(start_size < end_size);
+
     // TODO: Print number of years
+
+    printf("Number of Years: %i\n, years)
 }
